801805a8aa36c7222eaf508ed4a41043ef1ddf9a
Eric <linmingy@usc.edu>
2019-11-17 09:58:24 -0800

    add chart example for reference

--- tribe/src/app/app.component.ts
+++ tribe/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Car{
+  id : number;
+  manufacturer : string;
+  model : string;
+  displ : number;
+  year : number;
+  cyl : number;
+}
+
+const cars : Car[] = [
+  { id: 1, manufacturer: 'audi', model: 'a4', displ: 1.8, year: 1999, cyl: 4 },
+  { id: 2, manufacturer: 'audi', model: 'a4', displ: 2.0, year: 2008, cyl: 4 },
+  { id: 3, manufacturer: 'audi', model: 'a4', displ: 2.8, year: 1999, cyl: 6 },
+  { id: 4, manufacturer: 'audi', model: 'a4', displ: 3.1, year: 2008, cyl: 6 },
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +23,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tribe';
+  cars = cars;
 }
--- tribe/src/app/app.module.ts
+++ tribe/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    HeaderComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
