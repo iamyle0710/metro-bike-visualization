b1d97b3e6f605404c7d37b876579014ea99c7401
Eric <linmingy@usc.edu>
2019-11-21 00:20:43 -0800

    update modules and styles

--- tribe/src/app/app.module.ts
+++ tribe/src/app/app.module.ts
@@ -3,23 +3,36 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
 import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { MapService } from './core/services/map.service';
+
+const appRoutes = [
+  { path : '', component : ChartComponent },
+  { path : 'map', component : MapComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ChartComponent,
     HeaderComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    MapService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
