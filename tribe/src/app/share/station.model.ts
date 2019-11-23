7259cc1ee3950e516edeca4226143fcb962150b5
Eric <linmingy@usc.edu>
2019-11-23 10:53:01 -0800

    add station model

---
+++ tribe/src/app/share/station.model.ts
@@ -0,0 +1,14 @@
+export class StationStatus {
+    public name : string = '';
+    public bikesAvailable : number = 0;
+    public docksAvailable : number = 0;
+    public totalInNumber : number = 0;
+    public totalOutNumber : number = 0;
+
+    constructor(){
+        // this.name = name;
+        // this.bikesAvailable = bikesAvailable;
+        // this.docksAvailable = docksAvailable;
+    }
+
+}
