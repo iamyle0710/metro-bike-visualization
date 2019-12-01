41b6ffd7b399ffdfdd801f69988ed6c702c55be3
Eric <linmingy@usc.edu>
2019-12-01 13:20:15 -0800

    update header component

--- tribe/src/app/header/header.component.ts
+++ tribe/src/app/header/header.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'app-header',
-    templateUrl : './header.component.html'
+    templateUrl : './header.component.html',
+    styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent {
+    isCollapsed : boolean = false;
     constructor(){
         
     }
+    
 }
