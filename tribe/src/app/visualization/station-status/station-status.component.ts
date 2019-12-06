c58b5b597ed0e74c5871a9dd74dfb3cbd11feb8a
Eric <linmingy@usc.edu>
2019-12-05 21:46:09 -0800

    update station

--- tribe/src/app/visualization/station-status/station-status.component.ts
+++ tribe/src/app/visualization/station-status/station-status.component.ts
@@ -57,9 +57,7 @@ export class StationStatusComponent implements OnInit {
     });
   }
 
-  ngAfterViewInit() {
-    
-  }
+  ngAfterViewInit() {}
 
   updateData() {
     this.topFiveStations = this.topFiveStations.sort((a: any, b: any) => {
